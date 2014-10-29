3cc8082f1cc3c24daf98692d17fd9cbcbef70ade
John Pearson <jmxpearson@gmail.com>
2014-10-29 13:50:19 -0400

    minor edits to plotting code

--- plot_predicted_alphas.R
+++ plot_predicted_alphas.R
@@ -5,4 +5,6 @@ dat <- read.csv('Model1_preds.csv')
 df <- melt(dat[,2:3], id.vars=c())
 
 plt <- ggplot(df, aes(x=value, fill=variable))
-plt + geom_density(alpha=0.5)
+plt + geom_density(alpha=0.5) + xlab('Learning Rate') + ylab('Probability Density')
+
+ggsave('Model1_alpha_predicted_density.pdf', width=6, height=4, units='in')
